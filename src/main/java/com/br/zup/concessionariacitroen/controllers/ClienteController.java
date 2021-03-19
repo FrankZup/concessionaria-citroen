b33fcdaa2d76cbb440d7e6ed70bbbc5cf420bcfd
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-19 19:23:30 -0300

    Criar classe de ControllerCliente

---
+++ src/main/java/com/br/zup/concessionariacitroen/controllers/ClienteController.java
@@ -0,0 +1,33 @@
+package com.br.zup.concessionariacitroen.controllers;
+
+import com.br.zup.concessionariacitroen.models.Cliente;
+import com.br.zup.concessionariacitroen.services.ServiceCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    ServiceCliente serviceCliente;
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente cadastrarCliente(@RequestBody @Valid Cliente cliente) {
+        return serviceCliente.cadastrarCliente(cliente);
+    }
+
+    @GetMapping("{cpf}/")
+    public Cliente pesquisarCliente(@PathVariable String cpf) {
+        return serviceCliente.pesquisarClientePeloCPF(cpf);
+    }
+
+    public List<Cliente> listarClientes(){
+        return serviceCliente.listarClientes();
+    }
+}
