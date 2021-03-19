ce3099c097b065d0dafff7c0b45dcf51727659bb
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-19 17:37:15 -0300

    Iniciar o projeto

---
+++ src/main/java/com/br/zup/concessionariacitroen/ConcessionariaCitroenApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.concessionariacitroen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariaCitroenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionariaCitroenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zup/concessionariacitroen/ConcessionariaCitroenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.concessionariacitroen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaCitroenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
