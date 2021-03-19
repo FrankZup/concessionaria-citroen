4aeea861911294f516af49a96185f8831f69e989
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-19 17:57:19 -0300

    Criar método para adicionar cliente

---
+++ src/main/java/com/br/zup/concessionariacitroen/services/ServiceCliente.java
@@ -0,0 +1,18 @@
+package com.br.zup.concessionariacitroen.services;
+
+import com.br.zup.concessionariacitroen.models.Cliente;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ServiceCliente {
+
+    private static List<Cliente> clientes = new ArrayList<>();
+
+    public Cliente cadastrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+        return cliente;
+    }
+}
