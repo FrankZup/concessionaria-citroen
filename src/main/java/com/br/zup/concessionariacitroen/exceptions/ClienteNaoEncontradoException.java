775c8e32263621ab2c31ea3ad508838c2728e1ce
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-19 20:53:32 -0300

    Correção da exceção cliente não encontrado

--- src/main/java/com/br/zup/concessionariacitroen/exceptions/ClienteNaoEncontradoException.java
+++ src/main/java/com/br/zup/concessionariacitroen/exceptions/ClienteNaoEncontradoException.java
@@ -2,6 +2,6 @@ package com.br.zup.concessionariacitroen.exceptions;
 
 public class ClienteNaoEncontradoException extends ExcecaoBasica {
     public ClienteNaoEncontradoException(){
-        super("Cliente não encontrado", 422, null, "Unprocesable Entity");
+        super("Cliente não encontrado", 422, "cpf", "Unprocesable Entity");
     }
 }
