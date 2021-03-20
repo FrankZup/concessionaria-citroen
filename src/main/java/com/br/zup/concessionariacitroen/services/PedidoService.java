4668e954471d030cda3446af12720b4df4d2a0e9
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-03-20 18:44:12 -0300

    Adicionar a classe PedidoNaoEncontradoException no pacote exceptions

---
+++ src/main/java/com/br/zup/concessionariacitroen/services/PedidoService.java
@@ -0,0 +1,48 @@
+package com.br.zup.concessionariacitroen.services;
+
+import com.br.zup.concessionariacitroen.exceptions.PedidoNaoEncontradoException;
+import com.br.zup.concessionariacitroen.models.Carro;
+import com.br.zup.concessionariacitroen.models.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PedidoService {
+
+    private static List<Pedido> pedidos = new ArrayList<>();
+
+    @Autowired
+    CarroService carroService;
+    /**
+     *Método que permite o cadastro de um novo pedido, desde que tenha o carro em estoque
+     **/
+
+    public Pedido cadastrarPedido(Pedido pedido) {
+        pedidos.add(pedido);
+
+        Carro carro = pedido.getCarro();
+        carroService.removerCarroDoEstoque(carro);
+
+        return pedido;
+    }
+    /**
+     *Método que permite visualizar todos os pedidos cadastrados
+     **/
+    public List<Pedido> listarPedidos() {
+        return pedidos;
+    }
+    /**
+     *Método que permite pesquisar o pedido pelo número do pedido
+     **/
+    public Pedido pesquisarPedidoPeloNumero(int numeroPedido) {
+        for (Pedido pedido : pedidos) {
+            if (pedido.getNumeroPedido() == numeroPedido) {
+                return pedido;
+            }
+        }
+        throw new PedidoNaoEncontradoException();
+    }
+}
