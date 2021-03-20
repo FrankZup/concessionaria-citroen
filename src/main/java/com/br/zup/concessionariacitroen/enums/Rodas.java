b81c3f82aa620735385d31955adb0fd729ea153d
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-19 21:43:57 -0300

    Criar Enum Rodas

---
+++ src/main/java/com/br/zup/concessionariacitroen/enums/Rodas.java
@@ -0,0 +1,13 @@
+package com.br.zup.concessionariacitroen.enums;
+
+public enum Rodas {
+    SANSIRO_16("SANSIRO_16"),
+    DJOBE_17("DJOBE_17"),
+    DJOBE_17_DIAMANTADA("DJOBE_17_DIAMANTADA");
+
+    private String descricao;
+
+    Rodas(String descricao){
+        this.descricao = descricao;
+    }
+}
