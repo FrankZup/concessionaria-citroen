201d84e736ea9e2f35fb8b6b432f795c910b3649
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-19 21:41:00 -0300

    Criar Enum Motor

---
+++ src/main/java/com/br/zup/concessionariacitroen/enums/Motor.java
@@ -0,0 +1,11 @@
+package com.br.zup.concessionariacitroen.enums;
+
+public enum Motor {
+    THP_173_FLEX_2_0("THP_173_FLEX_2_0"),
+    VTI_1_6("VTI_1_6");
+
+    private String descricao;
+    Motor(String descricao){
+        this.descricao = descricao;
+    }
+}
